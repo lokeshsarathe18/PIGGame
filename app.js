f1aecf8473615e376b2deee05bb21dfab1f02d24
Lokesh Sarathe <41795050+lokeshsarathe18@users.noreply.github.com>
2019-04-24 12:52:25 +0530

    Fun Game in JavaScript

---
+++ app.js
@@ -0,0 +1,95 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScores, activePlayer, gamePlaying, lokesh, winningScore
+init()
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    if (gamePlaying) {
+        let dice = Math.floor(Math.random() * 6) + 1
+        let diceDOM = document.querySelector('.dice')
+        diceDOM.style.display = 'block'
+        diceDOM.src = 'dice-' + dice + '.png'
+        if (lokesh === dice && dice === 6) {
+            scores[activePlayer] = 0
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
+            nextPlayer()
+        } else if (dice !== 1) {
+            roundScores += dice
+            document.getElementById('current-' + activePlayer).textContent = roundScores
+        } else {
+            nextPlayer()
+        }
+        lokesh = dice
+    }
+})
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+        scores[activePlayer] += roundScores
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]
+
+        if (winningScore == 0) {
+            winningScore = 50
+        }
+        if (scores[activePlayer] >= winningScore) {
+
+            gamePlaying = false
+
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER !!!'
+            document.querySelector('.dice').style.display = 'none'
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+            // document.querySelector('.btn-roll').disabled = true
+            // document.getElementById('btn-hold').disabled = true
+        } else {
+            nextPlayer()
+        }
+    }
+})
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function nextPlayer() {
+    roundScores = 0
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    document.getElementById('current-0').textContent = 0
+    document.getElementById('current-1').textContent = 0
+
+    //document.querySelector('.player-0-panel').classList.remove('active')
+    //document.querySelector('.player-1-panel').classList.add('active')
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+}
+
+function init() {
+    scores = [0, 0]
+    roundScores = 0
+    activePlayer = 0
+    gamePlaying = true
+    lokesh = 0
+    winningScore = prompt('Enter the Winning Score')
+    document.querySelector('.dice').style.display = 'none'
+    document.getElementById('score-0').textContent = 0
+    document.getElementById('score-1').textContent = 0
+    document.getElementById('current-0').textContent = 0
+    document.getElementById('current-1').textContent = 0
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+
+    document.querySelector('.player-0-panel').classList.add('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+
+}
